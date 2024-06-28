72b9cbca3ba6af5eaaaf878542e25d36c0c508d8
enjia <enjia.wu@gmail.com>
2024-06-28 23:15:38 +0800

    updated user controller for practiccal

---
+++ practicals/PolytechnicLibraryAPI/models/user.js
@@ -0,0 +1,76 @@
+const sql = require("mssql");
+const dbConfig = require("../dbConfig");
+const bcrypt = require("bcryptjs");
+
+class User {
+    constructor(
+        user_id,
+        username,
+        passwordHash,
+        role
+    ) {
+        this.user_id = user_id;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
+
+    static async getAllUsers() {
+        try {
+            let pool = await sql.connect(dbConfig);
+            let users = await pool
+                .request()
+                .query("SELECT * FROM Users");
+            return users.recordset;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async getUserByUsername(username){
+        try {
+            let pool = await sql.connect(dbConfig);
+            let user = await pool
+                .request()
+                .input("username", username)
+                .query("SELECT * FROM Users WHERE username = @username");
+            return user.recordset[0];
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async createUser(username, password, role) {
+        try {
+            let pool = await sql.connect(dbConfig);
+            let passwordHash = bcrypt.hashSync(password, 10);
+            let user = await pool
+                .request()
+                .input("username", username)
+                .input("password", passwordHash)
+                .input("role", role)
+                .query(
+                    "INSERT INTO Users (username, passwordHash, role) VALUES (@username, @password, @role)"
+                );
+            return user;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static async deleteUser(user_id) {
+        try {
+            let pool = await sql.connect(dbConfig);
+            let user = await pool
+                .request()
+                .input("user_id", user_id)
+                .query("DELETE FROM Users WHERE user_id = @user_id");
+            return user;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+}
+
+module.exports = User;
