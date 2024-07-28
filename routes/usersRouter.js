eb5187f8c946e0e197e3fc1700f6f8e3bcb4f7ad
enjia <enjia.wu@gmail.com>
2024-07-28 18:04:19 +0800

    made minor edits

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -11,15 +11,16 @@ const usersController = require("../controllers/usersController.js");
 // Middleware
 const verifyJWT = require("../middlewares/verifyJWT.js");
 const upload = require("../middlewares/fileUpload.js");
+const {validateRegisterUser, validateLoginUser} = require('../middlewares/validateUser.js');
 
 //User Routes
 router.get("/allmember", usersController.getAllUsers); // testing data
-router.get("/profile/:userId", usersController.getUserByUserId); 
-router.put("/profile/:userId", upload.single('file'), usersController.updateUser);
+router.get("/profile/:userId", verifyJWT, usersController.getUserByUserId); 
+router.put("/profile/:userId", verifyJWT, upload.single('file'), usersController.updateUser);
 router.get("/count", usersController.getUserCount);
 router.get("/:username", usersController.getUserByUserName);
-router.post("/register", usersController.registerUser);
-router.post("/login", usersController.loginUser);
-router.post("/changePassword", usersController.changePassword);
+router.post("/register", validateRegisterUser, usersController.registerUser);
+router.post("/login", validateLoginUser, usersController.loginUser);
+router.post("/changePassword", verifyJWT, usersController.changePassword);
 
 module.exports = router; // Export the router
