20d30d52559cf9d62f2ebc4af426184d021c09dc
Timothy Chai <s10256132@connect.np.edu.sg>
2024-07-28 20:41:22 +0800

    Fix CSS conflicts

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -11,16 +11,24 @@ const usersController = require("../controllers/usersController.js");
 // Middleware
 const verifyJWT = require("../middlewares/verifyJWT.js");
 const upload = require("../middlewares/fileUpload.js");
-const {validateRegisterUser, validateLoginUser} = require('../middlewares/validateUser.js');
+const {
+    validateRegisterUser,
+    validateLoginUser,
+} = require("../middlewares/validateUser.js");
 
 //User Routes
 router.get("/allmember", usersController.getAllUsers); // testing data
-router.get("/profile/:userId", verifyJWT, usersController.getUserByUserId); 
-router.put("/profile/:userId", verifyJWT, upload.single('file'), usersController.updateUser);
+router.get("/profile/:userId", verifyJWT, usersController.getUserByUserId);
+router.put(
+    "/profile/:userId",
+    verifyJWT,
+    upload.single("file"),
+    usersController.updateUser
+);
 router.get("/count", usersController.getUserCount);
 router.get("/:username", usersController.getUserByUserName);
 router.post("/register", validateRegisterUser, usersController.registerUser);
-router.post("/login", validateLoginUser, usersController.loginUser);
+router.post("/login", usersController.loginUser);
 router.post("/changePassword", verifyJWT, usersController.changePassword);
 
 module.exports = router; // Export the router
