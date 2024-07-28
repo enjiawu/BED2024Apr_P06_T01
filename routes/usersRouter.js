8b6ad33281e9aad0510fbf98db367046c639b7d8
xuewenya <s10262410@connect.np.edu.sg>
2024-07-28 15:59:25 +0800

    update jwt

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -14,12 +14,12 @@ const upload = require("../middlewares/fileUpload.js");
 
 //User Routes
 router.get("/allmember", usersController.getAllUsers); // testing data
-router.get("/profile/:userId", usersController.getUserByUserId); 
-router.put("/profile/:userId", upload.single('file'), usersController.updateUser);
+router.get("/profile/:userId", verifyJWT,usersController.getUserByUserId); 
+router.put("/profile/:userId", verifyJWT, upload.single('file'), usersController.updateUser);
 router.get("/count", usersController.getUserCount);
 router.get("/:username", usersController.getUserByUserName);
 router.post("/register", usersController.registerUser);
 router.post("/login", usersController.loginUser);
-router.post("/changePassword", usersController.changePassword);
+router.post("/changePassword", verifyJWT, usersController.changePassword);
 
 module.exports = router; // Export the router
